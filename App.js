93f11955722a9d88dca198ccfb0fbacefde0898c
jafethmontero <jafethmontero@gmail.com>
2022-12-23 23:28:57 -0600

    sign in method with password and email

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, ActivityIndicator, TextInput } from "react-native";
 import TodoList from "./components/TodoList";
 import AddListModal from "./components/AddListModal";
 import { AntDesign } from "@expo/vector-icons";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 import colors from "./Colors";
 
 const App = () => {
   const [addTodoVisible, setAddTodoVisible] = useState(false);
+  const [loginModalVisible, setLoginModalVisible] = useState(true);
   const [lists, setLists] = useState([]);
-  const [user, setUser] = useState({});
-  const [userLoading, setUserLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [listsLoading, setListsLoading] = useState(true);
 
   const toggleAddTodoVisible = () => {
     setAddTodoVisible(!addTodoVisible);
+  };
+  const closeLoginModal = () => {
+    setLoginModalVisible(false);
   };
   const getLists = async () => {
     const collectionRef = collection(db, `users/${user.uid}/lists`);
@@ -55,35 +60,64 @@ const App = () => {
       alert(error.message);
     }
   };
-  const signIn = () => {
+  const signIn = async () => {
     const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-        setUserLoading(false);
-      } else {
-        signInAnonymously(auth)
-          .then(({ user }) => {
-            setUser(user);
-            setUserLoading(false);
-          })
-          .catch((error) => {
-            if (error) {
-              alert("Oh no, something went wrong");
-            }
-          });
-      }
-    });
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      setUser(user);
+      closeLoginModal();
+    } catch (error) {
+      alert(error.message);
+    }
+    // onAuthStateChanged(auth, (user) => {
+    //   if (user) {
+    //     setUser(user);
+    //     setUserLoading(false);
+    //   } else {
+    //     signInAnonymously(auth)
+    //       .then(({ user }) => {
+    //         setUser(user);
+    //         setUserLoading(false);
+    //       })
+    //       .catch((error) => {
+    //         if (error) {
+    //           alert("Oh no, something went wrong");
+    //         }
+    //       });
+    //   }
+    // });
   };
 
   useEffect(() => {
     if (user) {
       getLists();
     }
-    signIn();
   }, [user]);
 
-  return userLoading || listsLoading ? (
+  return !user ? (
+    <View style={styles.container}>
+      <Modal visible={loginModalVisible} animationType="slide" onRequestClose={() => closeLoginModal()}>
+        <TextInput
+          style={[styles.input, { borderColor: colors.pink }]}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={[styles.input, { borderColor: colors.pink }]}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          textContentType="password"
+        />
+        <TouchableOpacity style={styles.addList} onPress={() => signIn()}>
+          <AntDesign name="plus" size={16} color={colors.pink} />
+        </TouchableOpacity>
+      </Modal>
+    </View>
+  ) : listsLoading ? (
     <View style={styles.container}>
       <ActivityIndicator size="large" color={colors.turq} />
     </View>
@@ -93,7 +127,7 @@ const App = () => {
         <AddListModal closeModal={() => toggleAddTodoVisible()} addList={addList} />
       </Modal>
       <View>
-        <Text>User: {user.uid}</Text>
+        <Text>User: {user.email}</Text>
       </View>
       <StatusBar style="auto" />
 
@@ -158,6 +192,14 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     fontSize: 14,
     marginTop: 8,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 6,
+    marginRight: 8,
+    paddingHorizontal: 8,
   },
 });
 
